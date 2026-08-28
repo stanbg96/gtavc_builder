0f8290b6b6e308a2903c29deb3a096142abfd6f0
root <root@localhost.localdomain>
2026-08-28 15:50:50 +0300

    Add proper User-Agent headers and Overpass fallback mirrors

--- lib/services/osm_service.dart
+++ lib/services/osm_service.dart
@@ -3,7 +3,11 @@ import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
 
 class OsmService {
-  static const String overpassUrl = 'https://overpass-api.de/api/interpreter';
+  static const List<String> overpassEndpoints = [
+    'https://overpass-api.de/api/interpreter',
+    'https://overpass.kumi.systems/api/interpreter',
+    'https://maps.mail.ru/osm/tools/overpass/api/interpreter',
+  ];
 
   static Future<File> downloadOsmArea({
     required double minLat,
@@ -14,53 +18,65 @@ class OsmService {
   }) async {
     onProgress(0.1, 'Подготовка на Overpass заявка...');
 
-    final query = '''
-[out:xml][timeout:90];
-(
-  way["building"]($minLat,$minLon,$maxLat,$maxLon);
-  relation["building"]($minLat,$minLon,$maxLat,$maxLon);
-  way["highway"]($minLat,$minLon,$maxLat,$maxLon);
-);
-(._;>;);
-out body;
-''';
+    final query = '[out:xml][timeout:90];'
+        '('
+        'way["building"]($minLat,$minLon,$maxLat,$maxLon);'
+        'relation["building"]($minLat,$minLon,$maxLat,$maxLon);'
+        'way["highway"]($minLat,$minLon,$maxLat,$maxLon);'
+        ');'
+        '(._;>;);'
+        'out body;';
 
-    onProgress(0.2, 'Сваляне на OpenStreetMap данни от сървъра...');
-
-    final uri = Uri.parse(overpassUrl);
-    final client = http.Client();
-    final request = http.Request('POST', uri)
-      ..bodyFields = {'data': query};
-
-    final response = await client.send(request);
-
-    if (response.statusCode != 200) {
-      throw Exception('Грешка при сваляне на OSM: HTTP ${response.statusCode}');
-    }
+    onProgress(0.2, 'Сваляне на данни от OpenStreetMap...');
 
     final tempDir = await getTemporaryDirectory();
     final filePath = '${tempDir.path}/map_data_${DateTime.now().millisecondsSinceEpoch}.osm';
     final file = File(filePath);
-    final sink = file.openWrite();
 
-    int downloadedBytes = 0;
-    final contentLength = response.contentLength ?? -1;
+    Exception? lastError;
 
-    await response.stream.listen((chunk) {
-      sink.add(chunk);
-      downloadedBytes += chunk.length;
-      if (contentLength > 0) {
-        final progress = 0.2 + (downloadedBytes / contentLength) * 0.3;
-        onProgress(progress, 'Свалени ${(downloadedBytes / 1024).toStringAsFixed(1)} KB');
-      } else {
-        onProgress(0.35, 'Свалени ${(downloadedBytes / 1024).toStringAsFixed(1)} KB');
+    for (final endpoint in overpassEndpoints) {
+      try {
+        final uri = Uri.parse(endpoint);
+        final request = http.Request('POST', uri);
+        request.headers.addAll({
+          'User-Agent': 'GtaVcBuilderApp/1.0 (https://github.com/stanbg96/gtavc_builder)',
+          'Accept': '*/*',
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        });
+        request.bodyFields = {'data': query};
+
+        final response = await request.send();
+
+        if (response.statusCode == 200) {
+          final sink = file.openWrite();
+          int downloadedBytes = 0;
+          final contentLength = response.contentLength ?? -1;
+
+          await response.stream.listen((chunk) {
+            downloadedBytes += chunk.length;
+            sink.add(chunk);
+            if (contentLength > 0) {
+              final progress = 0.2 + (downloadedBytes / contentLength) * 0.3;
+              onProgress(progress, 'Свалени ${(downloadedBytes / 1024).toStringAsFixed(1)} KB');
+            } else {
+              onProgress(0.35, 'Свалени ${(downloadedBytes / 1024).toStringAsFixed(1)} KB');
+            }
+          }).asFuture();
+
+          await sink.flush();
+          await sink.close();
+
+          onProgress(0.5, 'OSM данните са свалени успешно!');
+          return file;
+        } else {
+          lastError = Exception('Сървърът $endpoint върна HTTP ${response.statusCode}');
+        }
+      } catch (e) {
+        lastError = Exception('Грешка при връзка с $endpoint: $e');
       }
-    }).asFuture();
+    }
 
-    await sink.flush();
-    await sink.close();
-
-    onProgress(0.5, 'OSM данните са свалени успешно!');
-    return file;
+    throw lastError ?? Exception('Неуспешно изтегляне на OSM данни');
   }
 }
